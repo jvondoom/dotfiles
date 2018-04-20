bb594421a559de63250ea6b147c0a3b7849f2eed
Juan von Doom <phazhion@gmail.com>
2018-04-20 00:20:59 -0600

    Added comments to subl.sh

--- bin/subl.sh
+++ bin/subl.sh
@@ -1,1 +1,5 @@
+## Configure Shell to open Sublime Text 3 from Terminal
+## Requires to change mode to executable
+## Run $ chmod u+x subl.sh
+
 sudo mkdir /usr/local/bin; ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl;
