b8413f64ada5457a6854fa5bbf3f9a9ceda4448e
Juan von Doom <phazhion@gmail.com>
2019-03-31 03:11:00 +0100

    Created bash script for zsh installation on fresh linux ditro

---
+++ scripts-linux/install-zsh.sh
@@ -0,0 +1,13 @@
+sudo apt-get install zsh -y
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+cp ../.zshrc ~/
+echo "file .zshrc copied to root address"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+cd
+mkdir this-temp-folder
+cd this-temp-folder
+git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
+cd awesome-terminal-fonts/
+cp -r build ~/.fonts
+echo "fonts copied"
+fc-cache -fv ~./fonts
