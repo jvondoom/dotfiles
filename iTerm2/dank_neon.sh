e6b4578e26e4cafbfe19e84afe2dea99463c9788
Juan von Doom <phazhion@gmail.com>
2019-09-04 21:36:46 -0600

    Removed the Dank Neon theme

--- iTerm2/dank_neon.sh
+++
@@ -1,5 +0,0 @@
-# /Users/jvondoom/Sites/dotfiles/iTerm2/dank_neon.itermcolors
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/foreground_color --type string "#EFF0F6"
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_color --type string "#191B2A"
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/bold_color --type string "#EFF0F6"
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/palette --type string "#191B2A:#AC3756:#2DAD86:#FFCA7A:#7496CC:#A37CA9:#01A8AD:#858DB7:#2A2D46:#FF476E:#39FFBA:#FFEE7A:#94BFFF:#F1B3F1:#01F7F7:#EFF0F6"
