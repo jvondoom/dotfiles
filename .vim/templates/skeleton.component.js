7d32cfe0b0ffa650a776e89c3e540d4614c1f7d7
Juan von Doom <phazhion@gmail.com>
2019-09-14 13:22:20 -0600

    Removed lint from JS shorcuts and templates

--- .vim/templates/skeleton.component.js
+++ .vim/templates/skeleton.component.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const x = props => {
   return (
-    <div className="<++>">
+    <div className='<++>'>
       <++>
     </div>
-  );
-};
+  )
+}
 
-export default <++>;
+export default <++>
