7c7977b9002e035f5afa43b63b2915e4199458e6
Juan von Doom <phazhion@gmail.com>
2019-10-05 08:54:26 -0600

    Updated the script with the new settings

--- scripts-linux/install-zsh.sh
+++ scripts-linux/install-zsh.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 #
+curl -L git.io/antigen > ~/antigen.zsh
+echo "zsh Antigen installed!"
 sudo apt-get install zsh -y
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cp ../.zshrc ~/
-echo "file .zshrc copied to root address"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-cd
-mkdir this-temp-folder
-cd this-temp-folder
-git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
-cd awesome-terminal-fonts/
-cp -r fonts ~/.fonts
-echo "fonts copied"
-cd
-rm -rf this-temp-folder
+echo "file .zshrc copied to home folder..."
+cp -r ../iTerm2/fonts/ ~/.fonts
+echo "fonts copied to home folder..."
